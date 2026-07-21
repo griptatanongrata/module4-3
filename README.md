85d301ab4fecb415b0af028b06e58439fdfa6275
griptatanongrata <j.ttipton@yahoo.com>
2026-07-21 17:08:20 -0500

    Module 4 Part 3

--- README.md
+++ README.md
@@ -7,3 +7,19 @@
 _Definition_
 
 -repeats the same code over and over again
+-the key is to follow DRY (Don't Repeat Yourself)
+-Types of Loops:
+1: for loop
+2: while loop
+3: do while loop
+
+_While_
+
+-creates a looping statement with regards to fixed variables
+-not used often
+
+_For_
+
+-creates a looping statement using specified variables and conditions
+-used often
+REMEMBER!: the % symbol in js shows if the first number is divisible by the second number
---
+++ index.js
@@ -0,0 +1,45 @@
+// Example of while loop //
+
+// let count = 1;
+
+//while (count <= 100) {
+//  console.log(count);
+//  count = count + 1;
+// }
+
+// Example of for loop //
+
+//for (let i = 0; i <= 100; i++) {
+// console.log(i);
+// }
+
+// For Loop Activity //
+
+// for (let i = 1; i <= 20; i++) {
+//  if (i % 3 === 0 && i % 5 === 0) {
+//    console.log(`Frontend Simplified`);
+// } else if (i % 3 === 0) {
+//    console.log(`Frontend`);
+//  } else if (i % 5 === 0) {
+//    console.log(`Simplified`);
+//  } else {
+//    console.log(`${i}`);
+//  }
+//}
+
+//for (let u = 1; u <= 100; u++) {
+//  if (u % 2 === 0 && u % 3 === 0) {
+//    console.log(`alright then`);
+//  } else if (u % 5 === 0) {
+//    console.log(`well alrighty`);
+//  } else {
+//    console.log(`ok`);
+//  }
+//. }
+
+// String "for loop" Activity //
+
+const str = "Frontend Simplified";
+for (let i = 0; i < str.length; i++) {
+  console.log(str[i]);
+}
